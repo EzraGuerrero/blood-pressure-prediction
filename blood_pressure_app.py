0a72b483e24a2d630cca3f18739d89cdcf890bb9
Ezra Guerrero <ezraguerreroglez@gmail.com>
2026-07-22 21:04:42 +0200

    Improved Model with BMI and Ethnicity; API request to get data, Fixed output directory for /models and /data

--- blood_pressure_app.py
+++ blood_pressure_app.py
@@ -5,26 +5,38 @@ import pandas as pd
 
 # Title
 st.title("🩺 Blood Pressure Prediction App")
-st.markdown("Predict systolic BP from age, gender, and BMI.")
+st.markdown("Predict systolic BP from age, gender, BMI, and ethnicity.")
 
 # Load model
-model = joblib.load("models/bp_model.pkl")
+model = joblib.load("../models/bp_model_improved.pkl")
 
 # Input form
 st.sidebar.header("Patient Input")
 age = st.sidebar.slider("Age (years)", 18, 100, 40)
 gender = st.sidebar.selectbox("Sex", ["Male", "Female"])
+bmi = st.sidebar.number_input("BMI (kg/m²)", 15.0, 50.0, 25.0)
+ethnicity = st.sidebar.selectbox("Ethnicity", [
+    "Mexican American", "Other Hispanic", "Non-Hispanic White", 
+    "Non-Hispanic Black", "Other Race - Including Multi-Racial"
+])
 
-# for improved model:
-# bmi = st.sidebar.number_input("BMI (kg/m2)", 15.0, 50.0, 25.0)
-
-# Encode gender
+# Encode inputs
 gender_code = 1 if gender == "Male" else 2
+ethnicity_map = {
+    "Mexican American": 1, "Other Hispanic": 2, "Non-Hispanic White": 3,
+    "Non-Hispanic Black": 4, "Other Race - Including Multi-Racial": 5
+}
+eth_code = ethnicity_map[ethnicity]
 
 # Create input DataFrame
 input_data = pd.DataFrame({
     "RIDAGEYR_scaled": [(age - 45.5) / 18.5], # Mean=45.5, std=18.5 (from training)
-    "RIAGENDR": [gender_code]
+    "RIAGENDR": [gender_code],
+    "BMXBMI_scaled": [(bmi - 28.0) / 6.0],  # Mean=28.0, std=6.0
+    "eth_2.0": [1 if eth_code == 2 else 0],
+    "eth_3.0": [1 if eth_code == 3 else 0],
+    "eth_4.0": [1 if eth_code == 4 else 0],
+    "eth_5.0": [1 if eth_code == 5 else 0]
 })
 
 # Predict
